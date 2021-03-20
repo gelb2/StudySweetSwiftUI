3395fde04c098b42f716c528bb9f2ad65e9d4e0e
JunYoungJee <rainmaker8492@gmail.com>
2021-03-20 13:41:16 +0900

    4. 프로퍼티로 뷰 추출하기

--- FruitMart/Source/User Interface/Home/ProductRow.swift
+++ FruitMart/Source/User Interface/Home/ProductRow.swift
@@ -11,48 +11,63 @@ import SwiftUI
 struct ProductRow: View {
     var body: some View {
         HStack {
-            Image("apple")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 140)
-                .clipped()
-            
-            VStack(alignment: .leading) {
-                Text("백설공주 사과")
-                    .font(.headline)
-                    .fontWeight(.medium)
-                    .padding(.bottom, 6)
-                
-                Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-                
-                Spacer()
-                
-                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0, content: {
-                    Text("₩").font(.footnote)
-                        + Text("2100").font(.headline)
-                    
-                    Spacer()
-                    
-                    Image(systemName: "heart")
-                        .imageScale(.large)
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32)
-                    
-                    Image(systemName: "cart")
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32)
-                })
-            }
-            .padding([.leading, .bottom], 12)
-            .padding([.top, .trailing])
+            productImage
+            productDescription
         }
         .frame(height: 150)
         .background(Color.primary.colorInvert())
         .cornerRadius(6)
         .shadow(color: Color.primary.opacity(0.33), radius: 1, x: 2, y: 2)
         .padding(.vertical, 8)
+    }
+}
+
+//기본 타입에서는 프로퍼티를 정의하고 뷰 프로토콜의 핵심인 body를 구현하는 작업만 담당하고 그 이외에는 확장 영역으로 구분한다
+//물론 다른 방법을 이용해서 코드를 정리하는 것도 괜찮다
+private extension ProductRow {
+    var productImage: some View {
+        Image("apple")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 140)
+            .clipped()
+    }
+
+    var productDescription: some View {
+        VStack(alignment: .leading) {
+            Text("백설공주 사과")
+                .font(.headline)
+                .fontWeight(.medium)
+                .padding(.bottom, 6)
+            
+            Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+            
+            Spacer()
+            
+            footerView
+        }
+        .padding([.leading, .bottom], 12)
+        .padding([.top, .trailing])
+    }
+
+    var footerView: some View {
+        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0, content: {
+            Text("₩").font(.footnote)
+                + Text("2100").font(.headline)
+            
+            Spacer()
+            
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+            
+            Image(systemName: "cart")
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+        })
     }
 }
 
